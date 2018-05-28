fbe98566b8ea3ba7036052ff0042cbc697b9575d
EDILBERTO MEJIA MEZA <EDILBERTO MEJIA MEZA@DESKTOP-9FRUUTC>
2018-05-27 23:10:31 -0500

    agregar todo los metodos de una calculadora

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -10,12 +10,53 @@ package calculadora;
  * @author EDILBERTO MEJIA MEZA
  */
 public class Calculadora {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private int ans=0;
+    
+    public Calculadora(){
+        ans=0;
     }
     
+    
+    public  int suma(int a, int b){
+        ans=a + b;
+        return ans;
+    }
+    public int resta(int a, int b){
+        ans=a-b;
+        return ans;
+    }
+    
+    public int div(int a, int b){
+        if (b==0){
+            throw new ArithmeticException("no se puede dividir por cero");
+        }
+        ans=a/b;
+        return ans;
+    }
+    
+    public int add(int v){
+        ans=ans+v;
+        return ans;
+    }
+    
+    public int sub(int v){
+        ans=ans-v;
+        return ans;
+    }
+    
+    public int ans(){
+        return ans;
+    }
+    
+    public void clear(){
+        ans=0;
+    }
+    
+    public void optima() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e){
+            
+        }
+    }
 }
